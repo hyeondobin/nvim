01b5ccd7f0c283260a4c9a049ad45b78322aee3f
hyeondobin <dobinhyeon@gmail.com>
2024-02-11 00:41:16 +0900

    add auto formatting on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,6 +3,7 @@ return {
 	lazy = false,
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		null_ls.setup({
 			sources = {
@@ -14,6 +15,19 @@ return {
 				null_ls.builtins.diagnostics.selene,
 				null_ls.builtins.diagnostics.eslint_d,
 			},
+
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format()
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
