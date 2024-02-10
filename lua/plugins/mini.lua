117cbde967515f84bf0e30472b61e0aee455ad44
hyeondobin <dobinhyeon@gmail.com>
2024-02-10 23:41:18 +0900

    replace plugins
    perisitence -> mini.sessions
    alpha -> mini.starter

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,19 +1,41 @@
 return {
+	{
+		"echasnovski/mini.files",
+		version = false,
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("mini.files").setup({
+				mappings = {
+					go_in = "<CR>",
+					go_out = "-",
+				},
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		version = false,
+		event = { "InsertEnter" },
+		config = true,
+	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		config = true,
+		event = { "InsertEnter" },
+	},
+	{
+		"echasnovski/mini.starter",
+		version = false,
+		config = function()
+			require("mini.starter").setup({
+                
+            })
+		end,
+	},
     {
-        "echasnovski/mini.files",
+        "echasnovski/mini.sessions",
         version = false,
-        event = { "BufReadPre", "BufNewFile" },
-    },
-    {
-        "echasnovski/mini.pairs",
-        version = false,
-        event = { "InsertEnter" },
         config = true,
-    },
-    {
-        "echasnovski/mini.surround",
-        version = false,
-        config = true,
-        event = { "InsertEnter" },
     },
 }
