63e069122775606658251a9a80fa2cc0effb6552
hyeondobin <dobinhyeon@gmail.com>
2024-02-11 16:10:17 +0900

    turn on autochdir

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,17 +37,15 @@ opt.undofile = true
 opt.backspace = "indent,eol,start"
 opt.splitright = true
 opt.splitbelow = true
-opt.autochdir = false
+opt.autochdir = true
 opt.iskeyword:append("-")
 opt.mouse:append("a")
 opt.clipboard:append("unnamedplus")
 opt.modifiable = true
 opt.encoding = "UTF-8"
 
-
 opt.isfname:append("@-@")
 
 opt.updatetime = 50
 opt.conceallevel = 0
 opt.ttimeoutlen = 200
-
