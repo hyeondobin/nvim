339a8c2e7ac6b3994a9837fa2ed3951387462c91
hyeondobin <dobinhyeon@gmail.com>
2024-02-12 21:59:57 +0900

    add some config to dressing

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,9 +1,26 @@
 return {
     {
         "stevearc/dressing.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim"
+        },
         lazy = false,
         config = function()
-            require("dressing").setup()
+            require("dressing").setup({
+                select = {
+                    get_config = function(opts)
+		if opts.kind == "codeaction" then
+			return {
+				backend = "nui",
+				nui = {
+					relative = "cursor",
+					max_width = 40,
+				},
+			}
+		end
+                    end,
+                },
+            })
         end,
     },
 }
