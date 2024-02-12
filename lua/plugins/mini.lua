33fe1504f911adfb24c36b05f7c227b2c39660e9
hyeondobin <dobinhyeon@gmail.com>
2024-02-12 21:36:27 +0900

    format

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,59 +1,59 @@
 return {
-    {
-        "echasnovski/mini.files",
-        version = false,
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("mini.files").setup({
-                mappings = {},
-                windows = {
-                    preview = true,
-                    width_preview = 100,
-                },
-            })
-        end,
-    },
-    {
-        "echasnovski/mini.pairs",
-        version = false,
-        event = { "InsertEnter" },
-        config = true,
-    },
-    {
-        "echasnovski/mini.surround",
-        version = false,
-        config = true,
-        event = { "InsertEnter" },
-    },
-    {
-        "echasnovski/mini.starter",
-        version = false,
-        config = function()
-            require("mini.starter").setup()
-        end,
-    },
-    {
-        "echasnovski/mini.sessions",
-        version = false,
-        config = function()
-            require("mini.sessions").setup()
-        end,
-    },
-    {
-        "echasnovski/mini.animate",
-        version = false,
-        config = function()
-            require("mini.animate").setup({
-                scroll = {
-                    enable = false,
-                },
-                open = {
-                    enable = false,
-                },
-                close = {
-                    enable = false,
-                },
-            })
-        end,
-    },
+	{
+		"echasnovski/mini.files",
+		version = false,
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("mini.files").setup({
+				mappings = {},
+				windows = {
+					preview = true,
+					width_preview = 100,
+				},
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		version = false,
+		event = { "InsertEnter" },
+		config = true,
+	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		config = true,
+		event = { "InsertEnter" },
+	},
+	{
+		"echasnovski/mini.starter",
+		version = false,
+		config = function()
+			require("mini.starter").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.sessions",
+		version = false,
+		config = function()
+			require("mini.sessions").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.animate",
+		version = false,
+		config = function()
+			require("mini.animate").setup({
+				scroll = {
+					enable = false,
+				},
+				open = {
+					enable = false,
+				},
+				close = {
+					enable = false,
+				},
+			})
+		end,
+	},
 }
