b02009773702ce27671aae300cf6ed803e5b9ffe
hyeondobin <dobinhyeon@gmail.com>
2024-02-12 21:12:03 +0900

    use lsp-zero

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,65 +1,73 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup({
-                ensuere_installed = {
-                    "selene"
-                },
-            })
-		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
 		lazy = false,
-        opts = {
-            automatic_installation = true,
-        },
+		branch = "v3.x",
+		config = function()
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_lspconfig()
+
+			lsp_zero.on_attach(function(_, bufnr)
+				lsp_zero.default_keymaps({ buffer = bufnr })
+				vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+			end)
+
+			lsp_zero.configure("gdscript", {
+				force_setup = true,
+				single_file_support = false,
+				cmd = { "ncat", "127.0.0.1", "6005" },
+				root_dir = require("lspconfig.util").root_pattern("project.godot", ".git"),
+				filetypes = { "gd", "gdscript", "gdscript3" },
+			})
+
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					lsp_zero.default_setup,
+
+					lua_ls = function()
+						require("lspconfig").lua_ls.setup({
+							settings = {
+								Lua = {
+									runtime = {
+										version = "LuaJIT",
+									},
+									diagnostics = {
+										globals = {
+											"vim",
+										},
+									},
+									workspace = {
+										library = vim.api.nvim_get_runtime_file("", true),
+									},
+									telemetry = {
+										enable = false,
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        event = { "BufReadPre", "BufNewFile" },
-		config = function()
-            -- variables
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            -- lua_ls config
-            lspconfig.lua_ls.setup ({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                            globals = {
-                                'vim',
-                            },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("",true),
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cssls.setup({})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-		end,
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		event = { "BufReadPre", "BufNewFile" },
 	},
 }
