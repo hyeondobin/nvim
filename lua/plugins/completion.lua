1b2fbe86c62d60b925339a80c04413d8ec6dd9af
hyeondobin <dobinhyeon@gmail.com>
2024-02-12 22:00:06 +0900

    fix nvim-cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -13,21 +13,28 @@ return {
 		dependencies = {
 			"neovim/nvim-lspconfig",
 			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
 			-- snippet
-			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
 			"rafamadriz/friendly-snippets",
+
+			-- icon
+			"onsails/lspkind.nvim",
 		},
 		config = function()
 			local cmp = require("cmp")
-                        local luasnip = require("luasnip")
+			local cmp_format = require("lsp-zero").cmp_format()
+			local luasnip = require("luasnip")
 
-            require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
+				preselect = "item",
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
@@ -42,19 +49,31 @@ return {
 					["<C-d>"] = cmp.mapping.scroll_docs(-4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-                    ["<C-h>"] = cmp.mapping(function ()
-                        luasnip.jump(-1)
-                    end),
-                    ["<C-l>"] = cmp.mapping(function ()
-                        luasnip.jump(1)
-                    end),
+					["<C-h>"] = cmp.mapping(function()
+						luasnip.jump(-1)
+					end),
+					["<C-l>"] = cmp.mapping(function()
+						luasnip.jump(1)
+					end),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "nvim_lua" },
 					{ name = "luasnip" },
+					{ name = "path" },
 				}, {
-					{ name = "buffer" },
+					{ name = "buffer", keyword_length = 3 },
 				}),
+
+				formatting = {
+					fields = { "menu", "abbr", "kind" },
+					format = require("lspkind").cmp_format({
+						mode = "symbol",
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
 			})
 
 			cmp.setup.filetype("gitcommit", {
@@ -74,8 +93,8 @@ return {
 			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
-					{ name = "path" },
 					{ name = "cmdline" },
+					{ name = "path" },
 				}),
 			})
 		end,
