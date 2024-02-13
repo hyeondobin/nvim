ca3e4e6bad9ea4c6a0c1a06ec0730e9eea401adb
hyeondobin <dobinhyeon@gmail.com>
2024-02-13 16:50:34 +0900

    format file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,70 +1,70 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        lazy = false,
-        branch = "0.1.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                -- enabled = false,
-                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-            },
-        },
-        config = function()
-            local actions = require("telescope.actions")
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = false,
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- enabled = false,
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+			},
+		},
+		config = function()
+			local actions = require("telescope.actions")
 
-            require("telescope").setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-p>"] = actions.move_selection_previous,
-                            ["<C-n>"] = actions.move_selection_next,
-                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                            ["<C-x>"] = actions.delete_buffer,
-                        },
-                    },
-                    file_ignore_patterns = {
-                        "node_modules",
-                        "yarn.lock",
-                        ".git",
-                        ".sl",
-                        "_build",
-                        ".next",
-                    },
-                    hidden = true,
-                    layout_config = {
-                        vertical = { width = 0.8 },
-                    },
-                },
-                extensions = {
-                    fzf = {
-                        fuzzy = true,
-                        override_generic_sorter = true,
-                        override_file_sorter = true,
-                        case_mode = "smart_case",
-                    }
-                }
-            })
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-p>"] = actions.move_selection_previous,
+							["<C-n>"] = actions.move_selection_next,
+							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+							["<C-x>"] = actions.delete_buffer,
+						},
+					},
+					file_ignore_patterns = {
+						"node_modules",
+						"yarn.lock",
+						".git",
+						".sl",
+						"_build",
+						".next",
+					},
+					hidden = true,
+					layout_config = {
+						vertical = { width = 0.8 },
+					},
+				},
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			})
 
-            require("telescope").load_extension('fzf')
+			require("telescope").load_extension("fzf")
 
-            -- Enable telescope fzf native, if installed
-            pcall(require("telescope").load_extension, "fzf")
-        end,
-    },
-    {
-        'nvim-telescope/telescope-ui-select.nvim',
-        lazy = false,
-        config = function()
-            require('telescope').setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {}
-                    }
-                }
-            })
-            require('telescope').load_extension('ui-select')
-        end,
-    },
+			-- Enable telescope fzf native, if installed
+			pcall(require("telescope").load_extension, "fzf")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		lazy = false,
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
