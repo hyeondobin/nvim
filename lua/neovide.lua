5cdc063a0197850b6c1e7ea3a64a7484df8f629d
hyeondobin <dobinhyeon@gmail.com>
2024-02-14 14:52:23 +0900

    format file

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,23 +1,23 @@
-local function set_ime(args)
-	if args.event:match("Enter$") then
-		vim.g.neovide_input_ime = true
-	else
-		vim.g.neovide_input_ime = false
-	end
-end
-local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
-
-vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
-	group = ime_input,
-	pattern = "*",
-	callback = set_ime,
-})
-
-vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
-	group = ime_input,
-	pattern = "[/==?]",
-	callback = set_ime,
-})
+-- local function set_ime(args)
+-- 	if args.event:match("Enter$") then
+-- 		vim.g.neovide_input_ime = true
+-- 	else
+-- 		vim.g.neovide_input_ime = false
+-- 	end
+-- end
+-- local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+--
+-- vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+-- 	group = ime_input,
+-- 	pattern = "*",
+-- 	callback = set_ime,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+-- 	group = ime_input,
+-- 	pattern = "[/==?]",
+-- 	callback = set_ime,
+-- })
 
 -- cursor trail
 vim.g.neovide_cursor_animation_length = 0.05
