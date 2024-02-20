aa8b6de59ec1015603d9583acf65276234ee4a6e
hyeondobin <dobinhyeon@gmail.com>
2024-02-20 13:55:55 +0900

    lazyload vim-godot and fix gdscript indentation

--- lua/plugins/godot.lua
+++ lua/plugins/godot.lua
@@ -1,5 +1,10 @@
 return {
-    {
-        "habamax/vim-godot",
-    },
+	{
+		"habamax/vim-godot",
+		event = "BufEnter *.gd",
+		config = function()
+			vim.opt_local.foldmethod = "expr"
+			vim.opt_local.indentexpr = ""
+		end,
+	},
 }
