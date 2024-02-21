55f3ba55d26ca7a3121058a61992089118594590
hyeondobin <dobinhyeon@gmail.com>
2024-02-21 17:44:58 +0900

    fix linux issue

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.sqlite_clib_path = "C:\\Users\\dobin\\Appdata\\local\\nvim\\sqlite3.dll"
+if vim.loop.os_uname().sysname == "Windows_NT" then
+	vim.g.sqlite_clib_path = "C:\\Users\\dobin\\Appdata\\local\\nvim\\sqlite3.dll"
+end
