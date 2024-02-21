9a3f1781e374b5483ecb0a376cb92a7b9169a623
hyeondobin <dobinhyeon@gmail.com>
2024-02-21 17:55:06 +0900

    press tab to complete

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -45,6 +45,12 @@ return {
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
 			local lspkind = require("lspkind")
+			local function has_words_before()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
 			require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -59,7 +65,9 @@ return {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
-				mapping = cmp.mapping.preset.insert({
+				mapping = {
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-u>"] = cmp.mapping.scroll_docs(4),
 					["<C-d>"] = cmp.mapping.scroll_docs(-4),
 					["<C-Space>"] = cmp.mapping.complete(),
@@ -70,7 +78,22 @@ return {
 					["<C-l>"] = cmp.mapping(function()
 						luasnip.jump(1)
 					end),
-				}),
+					["<CR>"] = cmp.mapping(function(fallback)
+						fallback()
+					end),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.confirm({ select = true })
+						elseif has_words_before() then
+							cmp.complete()
+							if #cmp.get_entries() == 1 then
+								cmp.confirm({ select = true })
+							end
+						else
+							fallback()
+						end
+					end),
+				},
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "nvim_lsp_signature_help" },
@@ -111,14 +134,30 @@ return {
 			})
 
 			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
+				mapping = {
+					["<Tab>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.confirm({ select = true })
+						end
+					end, { "i", "c" }),
+					["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+					["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				},
 				sources = {
 					{ name = "buffer" },
 				},
 			})
 
 			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
+				mapping = {
+					["<Tab>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.confirm({ select = true })
+						end
+					end, { "i", "c" }),
+					["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+					["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				},
 				sources = cmp.config.sources({
 					{ name = "cmdline" },
 					{ name = "path" },
