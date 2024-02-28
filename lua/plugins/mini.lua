947b9c186369f2f18bf7fd4dfb0a3bd0011d027d
hyeondobin <dobinhyeon@gmail.com>
2024-02-28 17:30:03 +0900

    remove mini.animate and try another scroll plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -29,26 +29,6 @@ return {
 		event = { "InsertEnter" },
 	},
 	{
-		"echasnovski/mini.animate",
-		version = false,
-		config = function()
-			require("mini.animate").setup({
-				cursor = {
-					enable = true,
-				},
-				scroll = {
-					enable = true,
-				},
-				open = {
-					enable = true,
-				},
-				close = {
-					enable = true,
-				},
-			})
-		end,
-	},
-	{
 		"echasnovski/mini.bufremove",
 		version = false,
 		keys = {
