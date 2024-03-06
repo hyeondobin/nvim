e9f3c27188c3efe2a9c3c6af5864a2e5b4d057f7
hyeondobin <dobinhyeon@gmail.com>
2024-03-07 02:13:20 +0900

    remove unused block

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -47,31 +47,6 @@ return {
 					null_ls.builtins.diagnostics.markdownlint.with({}),
 				},
 				debug = true,
-
-				on_attach = function(client, bufnr)
-					-- local augroup_id =
-					-- 	vim.api.nvim_create_augroup("FormatModificationsDocumentFormattingGroup", { clear = false })
-					-- vim.api.nvim_clear_autocmds({ group = augroup_id, buffer = bufnr })
-					-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-					-- 	group = augroup_id,
-					-- 	buffer = bufnr,
-					-- 	callback = function()
-					-- 		local lsp_format_modifications = require("lsp-format-modifications")
-					-- 		lsp_format_modifications.format_modifications(client, bufnr)
-					-- 	end,
-					-- })
-
-					-- if client.supports_method("textDocument/formatting") then
-					--     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					--     vim.api.nvim_create_autocmd("BufWritePre", {
-					--         group = augroup,
-					--         buffer = bufnr,
-					--         callback = function()
-					--             vim.lsp.buf.format()
-					--         end,
-					--     })
-					-- end
-				end,
 			})
 
 			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
