94ccffab71d7eb70b1e23b4c59c583c48c75b171
hyeondobin <dobinhyeon@gmail.com>
2024-10-11 00:22:45 +0900

    fix trouble commands

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,18 +1,19 @@
 return {
 	{
 		"folke/trouble.nvim",
-		cmd = { "TroubleToggle", "Trouble" },
+		cmd = { "Trouble" },
 		opts = { use_diagnostic_signs = true },
 		keys = {
-			{ "<leader>xx", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Document Diagnostics (Trouble)" },
-			{ "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace Diagnostics (Trouble)" },
-			{ "<leader>xL", "<cmd>TroubleToggle loclist<CR>", desc = "Location List (Trouble)" },
-			{ "<leader>xQ", "<cmd>TroubleToggle quickfix<CR>", desc = "Quickfix List (Trouble)" },
+			{ "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Document Diagnostics (Trouble)" },
+			{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" },
+			{ "<leader>xL", "<cmd>Trouble loclist<CR>", desc = "Location List (Trouble)" },
+			{ "<leader>xQ", "<cmd>Trouble quickfix<CR>", desc = "Quickfix List (Trouble)" },
+			{ "<leader>xs", "<cmd>Trouble symbols toggle focus=false" },
 			{
 				"[q",
 				function()
 					if require("trouble").is_open() then
-						require("trouble").previous({ skip_groups = true, jump = true })
+						require("trouble").prev({ skip_groups = true, jump = true })
 					else
 						local ok, err = pcall(vim.cmd.cprev)
 						if not ok then
