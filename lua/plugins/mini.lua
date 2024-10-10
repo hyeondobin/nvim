7020ba9faaea66f768bcb9c67444b49f0b781c0e
hyeondobin <dobinhyeon@gmail.com>
2024-10-11 02:51:44 +0900

    remove mini.files | disable mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,23 +1,7 @@
 return {
 	{
-		"echasnovski/mini.files",
-		version = false,
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("mini.files").setup({
-				mappings = {},
-				windows = {
-					preview = true,
-					width_preview = 100,
-				},
-			})
-		end,
-	},
-	{
 		"echasnovski/mini.pairs",
+		enabled = false,
 		version = false,
 		event = { "InsertEnter" },
 		config = true,
@@ -26,7 +10,7 @@ return {
 		"echasnovski/mini.surround",
 		version = false,
 		config = true,
-		event = { "InsertEnter" },
+		event = { "BufReadPost" },
 	},
 	{
 		"echasnovski/mini.bufremove",
