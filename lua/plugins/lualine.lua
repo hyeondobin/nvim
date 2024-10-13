6b604a6ff6b90a2bf7eb8a0178677be6d2c46c7c
hyeondobin <dobinhyeon@gmail.com>
2024-10-14 02:47:16 +0900

    show remote status to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,35 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
+		dependencies = {},
 		config = function()
+			local Job = require("plenary.job")
+			local function update_gstatus()
+				Job:new({
+					command = "git",
+					args = { "rev-list", "--left-right", "--count", "HEAD...@{upstream}" },
+					on_exit = function(job, _)
+						local res = job:result()[1]
+						if type(res) ~= "string" then
+							gstatus = { ahead = 0, behind = 0 }
+							return
+						end
+						local ok, ahead, behind = pcall(string.match, res, "(%d+)%s*(%d+)")
+						if not ok then
+							ahead, behind = 0, 0
+						end
+						gstatus = { ahead = ahead, behind = behind }
+					end,
+				}):start()
+			end
+
+			if _G.Gstatus_timer == nil then
+				_G.Gstatus_timer = vim.uv.new_timer()
+			else
+				_G.Gstatus_timer:stop()
+			end
+			_G.Gstatus_timer:start(0, 2000, vim.schedule_wrap(update_gstatus))
+
 			require("lualine").setup({
 				options = {
 					theme = "auto",
@@ -10,9 +38,11 @@ return {
 				},
 				sections = {
 					lualine_b = {
+						function()
+							return gstatus.ahead .. " " .. gstatus.behind .. ""
+						end,
 						"diff",
 						"diagnostics",
-						"branch",
 					},
 					lualine_c = {
 						{ "filename", path = 1 },
