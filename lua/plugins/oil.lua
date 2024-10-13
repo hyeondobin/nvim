bea48ef75abceabd5db5eed3d3a2a17defaa1994
hyeondobin <dobinhyeon@gmail.com>
2024-10-14 03:23:45 +0900

    add vcs status to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,22 +1,42 @@
 return {
-	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {
-		keymaps = {
-			["<Esc>"] = "actions.close",
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			columns = {
+				"icon",
+				"mtime",
+			},
+			skip_confirm_for_simple_edits = true,
+			watch_for_changes = true,
+			keymaps = {
+				["<Esc>"] = "actions.close",
+			},
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					if name == ".." then
+						return true
+					else
+						return false
+					end
+				end,
+			},
+			win_options = {
+				signcolumn = "yes:2",
+			},
 		},
-		view_options = {
-			show_hidden = true,
-			is_always_hidden = function(name, _)
-				if name == ".." then
-					return true
-				else
-					return false
-				end
-			end,
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
 		},
+		event = { "BufReadPre" },
 	},
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
-	event = { "BufReadPre" },
+	{
+		"SirZenith/oil-vcs-status",
+		dependencies = {
+			"stevearc/oil.nvim",
+		},
+		config = true,
+	},
 }
