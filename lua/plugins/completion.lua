358d0c1e9486f88c4b5712e387d3929a7cd2e56a
hyeondobin <dobinhyeon@gmail.com>
2024-10-14 16:13:29 +0900

    make c-n to complete when no completion provided

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -45,13 +45,6 @@ return {
 			local luasnip = require("luasnip")
 			local lspkind = require("lspkind")
 
-			local function has_words_before()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
-
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			return {
@@ -66,11 +59,16 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = {
-					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-n>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							cmp.complete()
+						end
+					end),
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-u>"] = cmp.mapping.scroll_docs(4),
 					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<C-h>"] = cmp.mapping(function()
 						luasnip.jump(-1)
@@ -81,18 +79,6 @@ return {
 					["<CR>"] = cmp.mapping(function(fallback)
 						fallback()
 					end),
-					-- ["<Tab>"] = cmp.mapping(function(fallback)
-					-- 	if cmp.visible() then
-					-- 		cmp.confirm({ select = true })
-					-- 	elseif has_words_before() then
-					-- 		cmp.complete()
-					-- 		if #cmp.get_entries() == 1 then
-					-- 			cmp.confirm({ select = true })
-					-- 		end
-					-- 	else
-					-- 		fallback()
-					-- 	end
-					-- end),
 					["<C-y>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })
