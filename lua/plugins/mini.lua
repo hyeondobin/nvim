90826a3ee8004ae957938e6d3918d5bbbfba8077
hyeondobin <dobinhyeon@gmail.com>
2024-10-14 16:38:38 +0900

    change surround keymap
    add stylua ignore end - gitsigns

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,6 +9,20 @@ return {
 	{
 		"echasnovski/mini.surround",
 		version = false,
+		opts = {
+			mappings = {
+				add = "gsa",
+				delete = "gsd",
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+
+				suffix_last = "l", -- Suffix to search with "prev" method
+				suffix_next = "n", -- Suffix to search with "next" method
+			},
+		},
 		config = true,
 		event = { "BufReadPost" },
 	},
