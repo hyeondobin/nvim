1bc0f46a5400849e54ad0636f27064b0bc3b5c21
hyeondobin <dobinhyeon@gmail.com>
2025-01-30 00:47:16 +0900

    add noice(inactive)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
 		import = "plugins",
 	},
 	change_detection = {
-		notify = false,
+		notify = true,
 	},
 	performance = {
 		rtp = {
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,43 @@
+return {
+	"folke/noice.nvim",
+	enabled = false,
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					any = {
+						{ find = "%d+L, %d+B" },
+						{ find = "; after #%d+" },
+						{ find = "; before #%d+" },
+					},
+				},
+				view = "mini",
+			},
+		},
+		presets = {
+			bottom_search = true,
+			command_palette = true,
+			long_message_to_split = true,
+			lsp_doc_border = true,
+		},
+	},
+	config = function(_, opts)
+		if vim.o.filetype == "lazy" then
+			vim.cmd([[messages clear]])
+		end
+		require("noice").setup(opts)
+	end,
+}
