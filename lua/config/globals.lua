b1caab9cee8c4cdb410c1bad6b52e51d7bce2906
hyeondobin <dobinhyeon@gmail.com>
2025-01-29 16:35:56 +0900

    remap leader key to ','

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 if vim.uv.os_uname().sysname == "Windows_NT" then
 	vim.g.sqlite_clib_path = "C:\\Users\\dobin\\Appdata\\local\\nvim\\sqlite3.dll"
 end
@@ -16,3 +16,4 @@ vim.g.rustaceanvim = {
 		end,
 	},
 }
+
--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,9 +1,27 @@
 return {
 	"Wansmer/treesj",
 	keys = {
-		{ "<space>m", desc = "treesj Toggle" },
-		{ "<space>j", desc = "treesj Join" },
-		{ "<space>s", desc = "treesj Split" },
+		{
+			"<leader>tm",
+			function()
+				require("treesj").toggle()
+			end,
+			desc = "treesj Toggle",
+		},
+		{
+			"<leader>tj",
+			function()
+				require("treesj").join()
+			end,
+			desc = "treesj Join",
+		},
+		{
+			"<leader>ts",
+			function()
+				require("treesj").split()
+			end,
+			desc = "treesj Split",
+		},
 	},
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	opts = {},
