9f30b4a5f3b9048ec43bf3706616133f3de5f257
Hyeon Dobin <77524773+hyeondobin@users.noreply.github.com>
2025-03-09 21:57:13 +0900

    change harpoon quick menu keymap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
 
         -- stylua: ignore start
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end,{desc ="Harpoon add file"})
-		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,{desc = "Harpoon show list"})
 		vim.keymap.set("n", "<M-n>", function() harpoon:list():select(1) end)
 		vim.keymap.set("n", "<M-e>", function() harpoon:list():select(2) end)
 		vim.keymap.set("n", "<M-i>", function() harpoon:list():select(3) end)
