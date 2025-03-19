33239514de3353062e7b8be338b8ae5135f7b690
hyeondobin <dobinhyeon@gmail.com>
2025-03-19 21:47:16 +0900

    add obsidian.nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,25 @@
+local obsidianDir = ""
+local JLPath = ""
+if vim.uv.os_uname().sysname == "Windows_NT" then
+	obsidianDir = "F:/Users/Dobin/Obsidian"
+	JLPath = "F:/Users/Dobin/Obsidian/JL"
+else
+	obsidianDir = vim.fn.expand("~") .. "Obsidian"
+	JLPath = vim.fn.expand("~") .. "Obsidian/JL"
+end
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	event = {
+		"BufReadPre " .. JLPath .. "/*.md",
+		"BufNewFile " .. JLPath .. "/*.md",
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{ name = "JL", path = JLPath },
+		},
+	},
+}
