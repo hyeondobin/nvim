d292db65ba2bf0ca21c9645f27e02382b5adb9cf
Hyeon Dobin <77524773+hyeondobin@users.noreply.github.com>
2025-03-20 15:49:14 +0900

    change completion plugin from blink to cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,9 +11,9 @@ if vim.uv.os_uname().sysname == "Windows_NT" then
 			event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 			opts = {},
 			dependencies = {
-				"saghen/blink.cmp",
+				-- "saghen/blink.cmp",
 				"williamboman/mason-lspconfig.nvim",
-				-- "hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-nvim-lsp",
 				"folke/neoconf.nvim",
 			},
 			config = function()
@@ -39,9 +39,9 @@ if vim.uv.os_uname().sysname == "Windows_NT" then
 					end,
 				})
 
-                -- blink
+				-- blink
 				-- local lsp_capabilities = require("blink-cmp").get_lsp_capabilities()
-                -- cmp
+				-- cmp
 				local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 				local default_setup = function(server)
@@ -113,8 +113,8 @@ else
 		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 		opts = {},
 		dependencies = {
-			"saghen/blink.cmp",
-			-- "hrsh7th/cmp-nvim-lsp",
+			-- "saghen/blink.cmp",
+			"hrsh7th/cmp-nvim-lsp",
 			"folke/neoconf.nvim",
 			"nvim-lua/lsp-status.nvim",
 		},
@@ -125,29 +125,50 @@ else
 				callback = function(event)
 					local opts = { buffer = event.buf }
 
-                    -- stylua: ignore start
-					vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, opts)
-					vim.keymap.set("n", "gd", function () Snacks.picker.lsp_definitions() end, opts)
-					vim.keymap.set("n", "gD", function () vim.lsp.buf.declaration() end, opts)
-					vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-					vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
-					vim.keymap.set("n", "gr", function () Snacks.picker.lsp_references() end, opts)
-					vim.keymap.set("n", "gh", function() vim.lsp.buf.signature_help() end, opts)
-                    vim.keymap.set("n", "<leader>cr", function() vim.lsp.buf.rename() end, { buffer = event.buf, desc = "LSP Rename" })
-					vim.keymap.set({ "n", "x" }, "<leader>cf", function() vim.lsp.buf.format({async = true}) end, {buffer = event.buf, desc = "LSP Format"})
-					vim.keymap.set( "n", "<leader>ca", function() vim.lsp.buf.code_action() end, { buffer = event.buf, desc = "LSP Code Action" })
-					-- stylua: ignore end
+					vim.keymap.set("n", "K", function()
+						vim.lsp.buf.hover()
+					end, opts)
+					vim.keymap.set("n", "gd", function()
+						Snacks.picker.lsp_definitions()
+					end, opts)
+					vim.keymap.set("n", "gD", function()
+						vim.lsp.buf.declaration()
+					end, opts)
+					vim.keymap.set("n", "gi", function()
+						vim.lsp.buf.implementation()
+					end, opts)
+					vim.keymap.set("n", "go", function()
+						vim.lsp.buf.type_definition()
+					end, opts)
+					vim.keymap.set("n", "gr", function()
+						Snacks.picker.lsp_references()
+					end, opts)
+					vim.keymap.set("n", "gh", function()
+						vim.lsp.buf.signature_help()
+					end, opts)
+					vim.keymap.set("n", "<leader>cr", function()
+						vim.lsp.buf.rename()
+					end, { buffer = event.buf, desc = "LSP Rename" })
+					vim.keymap.set({ "n", "x" }, "<leader>cf", function()
+						vim.lsp.buf.format({ async = true })
+					end, { buffer = event.buf, desc = "LSP Format" })
+					vim.keymap.set("n", "<leader>ca", function()
+						vim.lsp.buf.code_action()
+					end, { buffer = event.buf, desc = "LSP Code Action" })
 				end,
 			})
 
 			local lsp = require("lspconfig")
-			local my_lsp_capabilities = require("blink-cmp").get_lsp_capabilities()
+			-- -- blink
+			-- local lsp_capabilities = require("blink-cmp").get_lsp_capabilities()
+			-- cmp
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local hostname = vim.fn.hostname() -- get hostname
 
 			lsp.nixd.setup({
 				cmd = { "nixd" },
-				capabilities = my_lsp_capabilities,
+				capabilities = lsp_capabilities,
 				settings = {
 					nixd = {
 						nixpkgs = {
