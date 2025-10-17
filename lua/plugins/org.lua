11a7947bcca5cddec2b04c6d83c60acb160880da
hyeondobin <dobinhyeon@gmail.com>
2025-10-17 18:16:24 +0900

    add org mode

---
+++ lua/plugins/org.lua
@@ -0,0 +1,12 @@
+return {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+        -- Setup org mode
+        require('orgmode').setup({
+            org_agenda_files = '~/org/**/*',
+            org_default_notes_file = '~/org/refile.org',
+        })
+    end
+}
